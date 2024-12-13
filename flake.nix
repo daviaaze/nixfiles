469bebaf9e87ac9bbd6dbabd2debdb95cbe08d0e
Davi Azevedo <daviaaze@gmail.com>
2024-12-13 19:03:06 -0300

    Remove nur and activitywatch

--- flake.nix
+++ flake.nix
@@ -1,16 +1,15 @@
 {
   description = "My system configuration in flakes";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/5633bcff0c6162b9e4b5f1264264611e950c8ec7";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     fufexan.url = "github:fufexan/dotfiles";
-    lanzaboote.url = "github:nix-community/lanzaboote";
+    lanzaboote.url = "github:nix-community/lanzaboote/v0.4.1";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     luxuryescapes-cli.url = "path:/home/daviaaze/Projects/Lux/cli";
     zen-browser.url = "github:daviaaze/zen-browser-flake";
-    nur.url = github:nix-community/NUR;
   };
   outputs = { self, nixpkgs, home-manager, lanzaboote, ... }@inputs:
     {
@@ -23,7 +22,6 @@
           lanzaboote.nixosModules.lanzaboote
           home-manager.nixosModules.home-manager
           {
-            nixpkgs.overlays = [ inputs.nur.overlay ];
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
--- home/default.nix
+++ home/default.nix
@@ -1,27 +1,38 @@
 { inputs, pkgs, ... }: {
   imports = [
-    inputs.nur.hmModules.nur
     ./zsh.nix
     ./services.nix
     ./gnome.nix
     ./work.nix
     ./gaming.nix
     ./wayland.nix
-    ./activitywatch.nix
   ];
   home = {
     username = "daviaaze";
     homeDirectory = "/home/daviaaze";
     stateVersion = "23.11";
+    enableNixpkgsReleaseCheck = true;
+    
     packages = with pkgs; [
       spotify
       stremio
       trayscale
-      inputs.zen-browser.packages."${system}".default
+      discord-canary
+      inputs.zen-browser.packages.${pkgs.system}.default
+      code-cursor
     ];
   };
 
   programs = {
     home-manager.enable = true;
+  };
+
+  # Properly handle GTK themes under Wayland
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
   };
 }
