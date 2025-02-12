0caaf7a7d745761fe283ec3978293948c767e419
Davi Azevedo <daviaaze@gmail.com>
2025-02-12 16:43:23 -0300

    removed nix gc

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -15,12 +15,6 @@
       auto-optimise-store = true;
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-
     optimise = {
       automatic = true;
       dates = [ "weekly" ];
